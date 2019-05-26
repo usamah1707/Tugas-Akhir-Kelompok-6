33410508a88bf0cb2f671d6239de4f8a270157c5
yuvitri <yuvitri14@gmail.com>
2019-05-27 02:53:03 +0700

    repeat initial commit heehe

--- TA_6/src/test/java/com/apap/TA/TAApplicationTests.java
+++ TA_6/src/test/java/com/apap/TA/TAApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class Tutorial05ApplicationTests {
+public class TAApplicationTests {
 
 	@Test
 	public void contextLoads() {
