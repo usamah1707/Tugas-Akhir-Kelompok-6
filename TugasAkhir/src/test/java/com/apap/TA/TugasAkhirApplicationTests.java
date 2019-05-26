309898142500fc2afc9235c61f997c8cd14c77dc
yuvitri <yuvitri14@gmail.com>
2019-05-26 21:19:08 +0700

    initial commit

---
+++ TugasAkhir/src/test/java/com/apap/TA/TugasAkhirApplicationTests.java
@@ -0,0 +1,16 @@
+package com.apap.TA;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TugasAkhirApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
