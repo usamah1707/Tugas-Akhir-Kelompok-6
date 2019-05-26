5dfcd75808bddc2504f4c42cc2c6e0c2dcb416ab
yuvitri <yuvitri14@gmail.com>
2019-05-27 02:36:52 +0700

    repeat initial commit heeehe

---
+++ TA_6/src/main/java/com/apap/TA/TA_6Application.java
@@ -0,0 +1,13 @@
+package com.apap.TA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TA_6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TA_6Application.class, args);
+	}
+
+}
---
+++ TA_6/src/main/java/com/apap/TA/controller/PageController.java
@@ -0,0 +1,14 @@
+package com.apap.TA.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+	@RequestMapping("/")
+	public String home(Model model) {
+		return "home";
+	}
+
+}
