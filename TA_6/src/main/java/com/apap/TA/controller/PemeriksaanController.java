98482a622a0be876f313d69e629eeba50f77df38
Fadhlan Zakiri <fadhlanzakiri@gmail.com>
2019-06-01 19:59:20 +0700

    Fitur 7 dan 8 bekerja sedikit

--- TA_6/src/main/java/com/apap/TA/controller/PemeriksaanController.java
+++ TA_6/src/main/java/com/apap/TA/controller/PemeriksaanController.java
@@ -1,10 +1,12 @@
 package com.apap.TA.controller;
 
+import com.apap.TA.model.LabSuppliesModel;
 import com.apap.TA.model.PemeriksaanModel;
 import com.apap.TA.service.PemeriksaanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -16,11 +18,24 @@ public class PemeriksaanController {
     @Autowired
     private PemeriksaanService periksaService;
 
-    @RequestMapping(value = "/lab/pemeriksaan/permintaan", method = RequestMethod.GET)
+    @RequestMapping(value = "/lab/pemeriksaan", method = RequestMethod.GET)
     private String view(Model model) {
         List<PemeriksaanModel> list = periksaService.getAllJadwal();
         model.addAttribute("jadwal", list);
-        return "pemeriksaan";
+        return "view-pemeriksaan";
+    }
+    
+    @RequestMapping(value = "/lab/pemeriksaan/permintaan", method = RequestMethod.GET)
+    private String add(Model model) {
+        model.addAttribute("jadwal", new PemeriksaanModel());
+        return "add-pemeriksaan";
+    }
+    
+    @RequestMapping(value = "/lab/pemeriksaan/permintaan", method = RequestMethod.POST)
+    private String addJadwalSubmit(@ModelAttribute PemeriksaanModel periksa) {
+        periksaService.addJadwal(periksa);
+        return "add";
+
     }
 
 }
