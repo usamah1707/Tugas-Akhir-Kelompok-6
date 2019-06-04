fbb03e18944970d027691266366da779dd6f9f96
Yuvitri Annisa D <47783857+yuvitri@users.noreply.github.com>
2019-06-04 08:22:16 +0700

    Update JadwalJagaDb.java

--- TA_6/src/main/java/com/apap/TA/repository/JadwalJagaDb.java
+++ TA_6/src/main/java/com/apap/TA/repository/JadwalJagaDb.java
@@ -1,5 +1,6 @@
 package com.apap.TA.repository;
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,6 +9,5 @@ import com.apap.TA.model.JadwalJagaModel;
 
 public interface JadwalJagaDb extends JpaRepository <JadwalJagaModel,  Long>{
 	Optional <JadwalJagaModel> findById (int id);
-	Optional <JadwalJagaModel> findByTanggal (Date tanggal);
-
+	List<JadwalJagaModel> findByTanggal(Date tanggal);
 }
