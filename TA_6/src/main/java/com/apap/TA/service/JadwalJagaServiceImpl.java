bc559fffd876fb385c3be15be296e2ced7c94a54
Yuvitri Annisa D <47783857+yuvitri@users.noreply.github.com>
2019-06-04 08:23:41 +0700

    Update JadwalJagaServiceImpl.java

--- TA_6/src/main/java/com/apap/TA/service/JadwalJagaServiceImpl.java
+++ TA_6/src/main/java/com/apap/TA/service/JadwalJagaServiceImpl.java
@@ -1,4 +1,7 @@
 package com.apap.TA.service;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
@@ -31,16 +34,43 @@ public class JadwalJagaServiceImpl implements JadwalJagaService{
 		return jadwalJagaDb.findAll();
 	}
 
-	@Override
-	public Optional<JadwalJagaModel> findByTanggal(Date Tanggal) {
-		// TODO Auto-generated method stub
-		return jadwalJagaDb.findByTanggal(Tanggal);
-	}
+	
 
 	@Override
 	public Optional<JadwalJagaModel> getJadwalDetail(int id) {
 		// TODO Auto-generated method stub
 		return jadwalJagaDb.findById(id);
+	}
+
+	@Override
+	public List<JadwalJagaModel> findByTanggal(Date tanggal) {
+		// TODO Auto-generated method stub
+		return jadwalJagaDb.findByTanggal(tanggal);
+	}
+
+	@Override
+	public boolean validateJadwal(JadwalJagaModel jadwal) {
+		// TODO Auto-generated method stub	
+		boolean res = true; 
+		
+		LocalDate today = LocalDate.now();
+		java.sql.Date now = java.sql.Date.valueOf(today); 
+
+		java.util.TimeZone tz = java.util.TimeZone.getTimeZone("GMT+7");
+		Calendar cl = Calendar.getInstance(tz);
+		LocalTime nowTime = LocalTime.now();
+		java.sql.Time time = java.sql.Time.valueOf(cl.get(Calendar.HOUR_OF_DAY)+":"+cl.get(Calendar.MINUTE)+":00");
+
+		if (jadwal.getTanggal().equals(now)){
+				String timeString = jadwal.getWaktuMulai();
+				java.sql.Time timeIn = java.sql.Time.valueOf(timeString+":00");
+				System.out.println("TimeIn: "+timeIn.toString() );
+				System.out.println("TimeIn: "+time.toString() );
+				if(timeIn.before(time)){
+					res = false ;
+				}
+		}
+		return res;
 	}
 
 }
